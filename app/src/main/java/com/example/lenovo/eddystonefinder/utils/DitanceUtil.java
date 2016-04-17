8b2a2547755b9aeeb4dadebc39477627961c83d0
Gouravmoy Mohanty <gouravmoy.mohanty91@gmail.com>
2016-04-17 14:31:28 +0530

    Base Near Beacon Detection Completed

---
+++ app/src/main/java/com/example/lenovo/eddystonefinder/utils/DitanceUtil.java
@@ -0,0 +1,25 @@
+package com.example.lenovo.eddystonefinder.utils;
+
+import com.example.lenovo.eddystonefinder.beans.Beacons;
+
+import java.util.Map;
+
+/**
+ * Created by lenovo on 4/17/2016.
+ */
+public class DitanceUtil {
+    public static String getNearestBeacon(Map<String, Beacons> deviceToBeaconMap) {
+        String returnValue = "";
+        double minDistance = 200;
+        double currDistance = 0;
+        for (Map.Entry<String, Beacons> beaconsEntry : deviceToBeaconMap.entrySet()) {
+            Beacons eddystone = beaconsEntry.getValue();
+            currDistance = BeaconUtils.distanceFromRssi(eddystone.getRssi(), eddystone.getTxPower());
+            if (minDistance > currDistance) {
+                minDistance = currDistance;
+                returnValue = eddystone.getDeviceAddress();
+            }
+        }
+        return returnValue;
+    }
+}
